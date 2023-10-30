c03c1ded69760684a27f610275c893c61ef4500e
Suman Kumar <suman-k001@webreinvent.com>
2023-10-30 18:03:46 +0530

    Finishing up the minor changes

--- vaahcms/Modules/Basic/Routes/backend/routes-blogs.php
+++ vaahcms/Modules/Basic/Routes/backend/routes-blogs.php
@@ -1,6 +1,8 @@
 <?php
 
 
+use VaahCms\Modules\Basic\Http\Controllers\Backend\BlogsController;
+
 Route::group(
     [
         'prefix' => 'backend/basic/blogs',
@@ -13,62 +15,62 @@ Route::group(
         /**
          * Get Assets
          */
-        Route::get('/assets', 'BlogsController@getAssets')
+        Route::get('/assets',[BlogsController::class,'getAssets'])
             ->name('vh.backend.basic.blogs.assets');
         /**
          * Get List
          */
-        Route::get('/', 'BlogsController@getList')
+        Route::get('/',[BlogsController::class,'getList'])
             ->name('vh.backend.basic.blogs.list');
         /**
          * Update List
          */
-        Route::match(['put', 'patch'], '/', 'BlogsController@updateList')
+        Route::match(['put', 'patch'], '/', [BlogsController::class,'updateList'])
             ->name('vh.backend.basic.blogs.list.update');
         /**
          * Delete List
          */
-        Route::delete('/', 'BlogsController@deleteList')
+        Route::delete('/', [BlogsController::class,'deleteList'])
             ->name('vh.backend.basic.blogs.list.delete');
 
 
         /**
          * Fill Form Inputs
          */
-        Route::any('/fill', 'BlogsController@fillItem')
+        Route::any('/fill', [BlogsController::class,'fillItem'])
             ->name('vh.backend.basic.blogs.fill');
 
         /**
          * Create Item
          */
-        Route::post('/', 'BlogsController@createItem')
+        Route::post('/', [BlogsController::class,'createItem'])
             ->name('vh.backend.basic.blogs.create');
         /**
          * Get Item
          */
-        Route::get('/{id}', 'BlogsController@getItem')
+        Route::get('/{id}', [BlogsController::class,'getItem'])
             ->name('vh.backend.basic.blogs.read');
         /**
          * Update Item
          */
-        Route::match(['put', 'patch'], '/{id}', 'BlogsController@updateItem')
+        Route::match(['put', 'patch'], '/{id}', [BlogsController::class,'updateItem'])
             ->name('vh.backend.basic.blogs.update');
         /**
          * Delete Item
          */
-        Route::delete('/{id}', 'BlogsController@deleteItem')
+        Route::delete('/{id}', [BlogsController::class,'deleteItem'])
             ->name('vh.backend.basic.blogs.delete');
 
         /**
          * List Actions
          */
-        Route::any('/action/{action}', 'BlogsController@listAction')
+        Route::any('/action/{action}', [BlogsController::class,'listAction'])
             ->name('vh.backend.basic.blogs.list.actions');
 
         /**
          * Item actions
          */
-        Route::any('/{id}/action/{action}', 'BlogsController@itemAction')
+        Route::any('/{id}/action/{action}', [BlogsController::class,'itemAction'])
             ->name('vh.backend.basic.blogs.item.action');
         //---------------------------------------------------------
 
