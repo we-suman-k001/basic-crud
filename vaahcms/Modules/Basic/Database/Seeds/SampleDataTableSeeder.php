b33313e606dee22a1ef8bbcbb1c6debff1b2216c
Suman Kumar <suman-k001@webreinvent.com>
2023-11-01 12:16:04 +0530

    added feature to add 15 sample data

--- vaahcms/Modules/Basic/Database/Seeds/SampleDataTableSeeder.php
+++ vaahcms/Modules/Basic/Database/Seeds/SampleDataTableSeeder.php
@@ -3,6 +3,10 @@ namespace VaahCms\Modules\Basic\Database\Seeds;
 
 
 use Illuminate\Database\Seeder;
+use Illuminate\Http\Request;
+use JetBrains\PhpStorm\NoReturn;
+use VaahCms\Modules\Basic\Models\Blog;
+use WebReinvent\VaahCms\Libraries\VaahSeeder;
 
 class SampleDataTableSeeder extends Seeder
 {
@@ -11,19 +15,22 @@ class SampleDataTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    #[NoReturn] public function run()
     {
-        $this->seeds();
-    }
-
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    function seeds()
-    {
-
+//        $request = new Request([
+//            'model_namespace' => Blog::class
+//        ]);
+//        $fillable = VaahSeeder::fill($request);
+//        $inputs = $fillable['data']['fill'];
+        $i = 0;
+        $records = 15;
+        while ($i < $records) {
+            $inputs = Blog::fillItem(false);
+            $item = new Blog();
+            $item->fill($inputs);
+            $item->save();
+            $i++;
+        }
     }
 
 
